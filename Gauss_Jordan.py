1906b98ff369b7a8d6437f38f107feb176b6d23b
Angel Avitia <avitianarvaez.angel@gmail.com>
2026-09-06 23:08:03 -0600

    Add files via upload

---
+++ Gauss_Jordan.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 27 18:19:48 2025
+
+@author: AVITIA
+"""
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+# eliminacion gausiana
+def E_GJ(A):
+
+    # filas de A 
+    f = A.shape[0] 
+    
+    for j in range(f):  
+        if A[j, j] == 0:
+            print("Existe un cero en la diagonal")
+            break
+        
+        A[j, j:] = A[j, j:] / A[j, j] 
+
+        for i in range(j + 1, f): 
+
+            A[i, j:] = - A[j, j:]*A[i, j] + A[i, j:]
+
+
+    return A
+
+
+def E_A(U):
+    
+    f2 = U.shape[0]
+    
+    for j in range(f2 - 1, -1, -1):
+          
+        for i in range(j - 1, -1, -1):
+            
+            U[i, j:] = -  U[i, j]*U[j,j:] + U[i, j:]
+            
+     return U
+            
+
+M = np.array([[5, 3, -1, 9],[3, 2, -1, 5],[1, 1, 1, -1]]).astype(float)
+
+
+U = E_GJ(M)
+
+print("La matriz triangular reducida es:")
+print(U)
+
+X = E_A(U)
+
+X1 = X[:,3]
+print("Vector solucion")
+print(X1)
+
